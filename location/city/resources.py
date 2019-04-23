3346a2661995adfb3d2d5851ea5ff70138e11943
anton.olivson@gmail.com <tohalivs11>
2019-04-23 11:45:45 +0300

    Merge branch 'RebuildGustoApiStep1' of https://gitlab.com/lantrix-fw/gusto_api into RebuildGustoApiStep2
    # Conflicts:
    #	gusto_api/models.py
    #	requirements.txt

--- location/city/resources.py
+++ location/city/resources.py
@@ -1,30 +1,41 @@
 import falcon
 from mongoengine.errors import ValidationError
-from auth.resources import Resource
+from falcon_core.resources import Resource
 from auth.utils import get_request_multiple, get_request_single, delete_request
-from gusto_api.models import Cities, Permissions
+from gusto_api.models import Cities
+from gusto_api.utils import filter_queryset, dict_from_model
 
 
 class CitiesResource(Resource):
 
     use_token = True
 
+    cities_template = (
+        ('name', 'string'),
+        ('country_code', 'string'),
+        ('default', 'boolean'),
+        ('active', 'boolean'),
+        ('lat', 'integer'),
+        ('lng', 'integer'),
+        ('number_phone', 'string'),
+        ('language', 'object', (
+            ('en', 'string'),
+            ('ru', 'string'),
+            ('uk', 'string'),
+        ),),
+        ('exist_store', 'boolean'),
+    )
+
     def on_get(self, req, resp, **kwargs):
-        # city = Permissions.objects.all().first()
-        # print(city._meta['collection'])
-        get_request_multiple(Cities, req.params, resp)
+        cities = filter_queryset(Cities.objects, **req.params)
+        resp.media = dict_from_model(cities, self.cities_template, iterable=True)
         resp.status = falcon.HTTP_200
 
-    def on_post(self, req, resp, **kwargs):
+    def post(self, req, resp, data, **kwargs):
         try:
-            data = req.stream.read()
-            if not data:
-                resp.status = falcon.HTTP_400
-                return
-            data = falcon.json.loads(data)
-
             city = Cities(**data)
             city.save()
+            resp.media = dict_from_model(city, CitiesResource.cities_template)
             resp.status = falcon.HTTP_201
         except ValidationError:
             resp.status = falcon.HTTP_400
@@ -35,18 +46,12 @@ class CityResource(Resource):
     def on_get(self, req, resp, **kwargs):
         get_request_single(Cities, resp, **kwargs)
 
-    def on_put(self, req, resp, **kwargs):
+    def put(self, req, resp, data, **kwargs):
         city = Cities.objects.filter(**kwargs).first()
         if city is None:
             resp.status = falcon.HTTP_404
             return
         try:
-            data = req.stream.read()
-            if not data:
-                resp.status = falcon.HTTP_400
-                return
-            data = falcon.json.loads(data)
-
             city.update(**data)
             resp.status = falcon.HTTP_200
         except ValidationError:
